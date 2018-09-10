e845da84a071487fa1b8f2bf320de83d7ba20f8b
gus mac <gusmac@gmail.com>
2018-09-11 07:48:26 +0800

    updated index page and controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
 before_action :set_post, only: [:edit, :update, :destroy, :show]
 
   def index
+    @posts = Post.all.order("created_at DESC")
   end
 
   def new
